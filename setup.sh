1d3555cb43000ca5685cd5e143e10cca779b2a26
N8Deathrider <nath001c@gmail.com>
2022-06-02 11:00:34 -0600

    Added creation of the config file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 FOLDER=${HOME}/.ssh
 CONFIG=${FOLDER}/config
+SSH_CONFIG_TEXT="MACs hmac-md5,hmac-sha1,umac-64@openssh.com,hmac-sha2-512
+HostkeyAlgorithms ssh-dss,ssh-rsa
+ServerAliveInterval 30
+StrictHostKeyChecking=accept-new
+KexAlgorithms +diffie-hellman-group-exchange-sha1,diffie-hellman-group14-sha1,diffie-hellman-group1-sha1
+Ciphers +aes128-cbc,3des-cbc,aes192-cbc,aes256-cbc"
 DEFAULT_COLOR_FG="\033[39m"
 GREEN_ON="\033[32m"
 YELLOW_ON="\033[33m"
@@ -25,12 +31,11 @@ else
     FOLDER_EXIT_CODE=$?
     if [ ${FOLDER_EXIT_CODE} -ne 0 ]
     then
-        printf "[${RED_ON}!${DEFAULT_COLOR_FG}] UhOh something has gone wrong\n"
+        printf "[${RED_ON}!!!${DEFAULT_COLOR_FG}] UhOh something has gone wrong\n"
         exit ${FOLDER_EXIT_CODE}
-    elif [ ${FOLDER_EXIT_CODE} -eq 0 ]
-    then
+    else
         sleep 2
-        printf "[${GREEN_ON}i${DEFAULT_COLOR_FG}] ${FOLDER} created successfully\n"
+        printf "[${GREEN_ON}i${DEFAULT_COLOR_FG}] The folder ${FOLDER} has been created successfully\n"
     fi
 fi
 
@@ -43,6 +48,16 @@ else
     printf "[${YELLOW_ON}!${DEFAULT_COLOR_FG}] The file ${CONFIG} does not yet exist\n"
     sleep 3
     printf "[${GREEN_ON}i${DEFAULT_COLOR_FG}] Creating the file ${CONFIG} now\n"
+    echo "${SSH_CONFIG_TEXT}" > ${CONFIG}
+    CONFIG_EXIT_CODE=$?
+    if [ ${CONFIG_EXIT_CODE} -ne 0 ]
+    then
+        printf "[${RED_ON}!!!${DEFAULT_COLOR_FG}] UhOh something has gone wrong\n"
+        exit ${CONFIG_EXIT_CODE}
+    else
+        sleep 2
+        printf "[${GREEN_ON}i${DEFAULT_COLOR_FG}] The file ${CONFIG} has been created successfully\n"
+    fi    
 fi
     # prompt user if they would like to append to the existing $CONFIG
     # or if they would like to backup the existing $CONFIG
